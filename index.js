046c3af00c5e1ae0a2564e1551173126d78bff67
Ann-tech <Onyekaann17@gmail.com>
2023-08-09 04:43:49 +0100

    add initial configuration - logger and error middlewares

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+
+const errorHandlerMiddleware = require('./middlewares/errorHandler.middleware')
+const morganMiddleware = require('./middlewares/morgan.middleware');
+
+const logger = require('./logging/logger');
+
+require('dotenv').config();
+
+// Add the morgan middleware
+app.use(morganMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+//To parse url encoded data
+app.use(express.urlencoded( {extended: false} ))
+
+//To parse data passed via body
+app.use(express.json())
+
+app.use( helmet() )
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        logger.info(`Server is running on PORT ${PORT}`)
+    })
+}
+
+app.use( errorHandlerMiddleware )
+
+module.exports = app;
