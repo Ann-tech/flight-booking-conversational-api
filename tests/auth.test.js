f9be4540eb4f43d8187ee9a417da860f769eef27
Ann-tech <Onyekaann17@gmail.com>
2023-08-09 19:52:49 +0100

    test for ommitted data

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -5,11 +5,22 @@ describe('Test POST /api/v1/auth/signup', () => {
     const users = require('./fixtures/users.json');
 
     it('should return 201 success', async () => {
+        let userWithCompleteData = users[0];
         const response = await request(app).post('/api/v1/auth/signup')
-                .send(users[0])
+                .send(userWithCompleteData)
                 .expect(201)
                 .expect("Content-Type", /json/);
         expect(response.body.success).toBe(true);
         expect(response.body.message).toBe("signup successful");                  
+    });
+
+    it('should return 400 error', async () => {
+        let userWithoutCompleteData = users[1];
+        const response = await request(app).post('/api/v1/auth/signup')
+                .send(userWithoutCompleteData)
+                .expect(400)
+                .expect("Content-Type", /json/);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe("provide required fields");                  
     });
 })
