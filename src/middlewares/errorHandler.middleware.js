b3dc2ccd0bc02082699692ed668b2ffc7c16d481
Ann-tech <Onyekaann17@gmail.com>
2023-08-09 04:50:24 +0100

    clean up setup and add error and logging functionalities

---
+++ src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,35 @@
+const logger = require('../src/logging/logger')
+
+const handleDuplicateKeyError = (err, res) => {
+    const field = Object.keys(err.keyValue);
+    const code = 409;
+    const error = `${field} already exists`;
+    res.status(code).json({
+        success: false,
+        fields: field.join(", "),
+        message: error
+    });
+}
+
+const handleValidationError = (err, res) => {
+    let errors = Object.values(err.errors).map(el => el.message);
+    let fields = Object.values(err.errors).map(el => el.path);
+    let code = 400;
+
+    const formattedErrors = errors.join(' ');
+
+    res.status(code).json({success: false, message: formattedErrors, fields: fields.join(', ')});
+}
+
+async function errorHandler(err, req, res, next) {
+    logger.error("An error has occured")
+    logger.error(err.message)
+    try {
+        if(err.name === 'ValidationError') return handleValidationError(err, res);
+        if(err.code && err.code == 11000) return handleDuplicateKeyError(err, res);
+    } catch(err) {
+        res.status(500).json({success: false, message: 'An unknown error occurred.'});
+    }
+}
+
+module.exports = errorHandler;
