5732508c879e684e171ed960b30ad8c6a11c8b73
Ann-tech <Onyekaann17@gmail.com>
2023-08-09 21:13:03 +0100

    add data for missing flight field

--- tests/flights.test.js
+++ tests/flights.test.js
@@ -4,24 +4,26 @@ const app = require('../src/index');
 describe('Test POST /api/v1/auth/signup', () => {
     const flights = require('./fixtures/flights.json');
     const users = require('./fixtures/users.json');
+    let loginAsAdmin = users[5];
+
+    beforeEach(async () => {
+        const response = await request(app).post('/api/v1/auth/login')
+                .send(loginAsAdmin)
+        ({ token } = response.body);
+    })
 
     it('should return 201 success', async () => {
         let flight = flights[0];
-        let loginAsAdmin = users[5];
 
-        const response = await request(app).post('/api/v1/auth/login')
-                .send(loginAsAdmin)
-        const { token } = response.body;
-
-        const response2 = await request(app).post('/api/v1/flights')
+        const response = await request(app).post('/api/v1/flights')
                 .set({
                     "Authorization": `Bearer ${token}`
                 })
                 .send(flight)
                 .expect(201)
                 .expect("Content-Type", /json/);
-        expect(response2.body.success).toBe(true);
-        expect(response2.body.message).toBe("flight successfully scheduled");                  
+        expect(response.body.success).toBe(true);
+        expect(response.body.message).toBe("flight successfully scheduled");                  
     });
 
 })
