30b9541cbbeed65176e129e3f4098c7ee719ba06
Ann-tech <Onyekaann17@gmail.com>
2023-08-09 21:17:36 +0100

    test for missing flight fields

--- tests/flights.test.js
+++ tests/flights.test.js
@@ -13,17 +13,32 @@ describe('Test POST /api/v1/auth/signup', () => {
     })
 
     it('should return 201 success', async () => {
-        let flight = flights[0];
+        let flightWithCompleteData = flights[0];
 
         const response = await request(app).post('/api/v1/flights')
                 .set({
                     "Authorization": `Bearer ${token}`
                 })
-                .send(flight)
+                .send(flightWithCompleteData)
                 .expect(201)
                 .expect("Content-Type", /json/);
         expect(response.body.success).toBe(true);
         expect(response.body.message).toBe("flight successfully scheduled");                  
     });
+
+    it('should return 400 error', async () => {
+        let flightWithoutCompleteData = flights[1];
+
+        const response = await request(app).post('/api/v1/flights')
+                .set({
+                    "Authorization": `Bearer ${token}`
+                })
+                .send(flightWithoutCompleteData)
+                .expect(400)
+                .expect("Content-Type", /json/);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe("provide required fields");                  
+    });
+    
 
 })
