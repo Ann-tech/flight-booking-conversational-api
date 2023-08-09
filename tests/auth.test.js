029c3bcdd2b9a2794bed839f573d734d66b24595
Ann-tech <Onyekaann17@gmail.com>
2023-08-09 19:58:21 +0100

    test login route - successful login

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -5,9 +5,9 @@ describe('Test POST /api/v1/auth/signup', () => {
     const users = require('./fixtures/users.json');
 
     it('should return 201 success', async () => {
-        let userWithCompleteData = users[0];
+        let userWithCompleteSignupData = users[0];
         const response = await request(app).post('/api/v1/auth/signup')
-                .send(userWithCompleteData)
+                .send(userWithCompleteSignupData)
                 .expect(201)
                 .expect("Content-Type", /json/);
         expect(response.body.success).toBe(true);
@@ -15,12 +15,22 @@ describe('Test POST /api/v1/auth/signup', () => {
     });
 
     it('should return 400 error', async () => {
-        let userWithoutCompleteData = users[1];
+        let userWithoutSignupCompleteData = users[1];
         const response = await request(app).post('/api/v1/auth/signup')
-                .send(userWithoutCompleteData)
+                .send(userWithoutSignupCompleteData)
                 .expect(400)
                 .expect("Content-Type", /json/);
         expect(response.body.success).toBe(false);
         expect(response.body.message).toBe("provide required fields");                  
+    });
+
+    it('should return 200 success', async () => {
+        let userWithCompleteLoginData = users[2];
+        const response = await request(app).post('/api/v1/auth/login')
+                .send(userWithCompleteLoginData)
+                .expect(200)
+                .expect("Content-Type", /json/);
+        expect(response.body.success).toBe(true);
+        expect(response.body).toHaveProperty("token");                  
     });
 })
