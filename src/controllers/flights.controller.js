bdca7625a555ef500a70397f29ca09407f51eec4
Ann-tech <Onyekaann17@gmail.com>
2023-08-11 18:11:50 +0100

    add create new flight feature

--- src/controllers/flights.controller.js
+++ src/controllers/flights.controller.js
@@ -2,7 +2,17 @@ const { Flight } = require('../models/db.connect');
 
 async function httpGetAllFlights(req, res, next) {
     try {
-        const flight = Flight.findAll();
+        const flight = await Flight.findAll();
+        res.json({success: true, message: 'flight successfully scheduled'})
+    } catch(err) {
+        console.log(err);
+        next(err);
+    }
+}
+
+async function httpCreateNewFlight(req, res, next) {
+    try {
+        const flight = await Flight.create(req.body);
         res.json({success: true, message: 'flight successfully scheduled'})
     } catch(err) {
         console.log(err);
