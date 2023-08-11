07b488ed3024bdd1c5915068080afca47a8fa5a1
Ann-tech <Onyekaann17@gmail.com>
2023-08-11 18:14:51 +0100

    add get flight by id

--- src/controllers/flights.controller.js
+++ src/controllers/flights.controller.js
@@ -2,8 +2,18 @@ const { Flight } = require('../models/db.connect');
 
 async function httpGetAllFlights(req, res, next) {
     try {
-        const flight = await Flight.findAll();
-        res.json({success: true, message: 'flight successfully scheduled'})
+        const flights = await Flight.findAll();
+        res.status(200).json({success: true, flights })
+    } catch(err) {
+        console.log(err);
+        next(err);
+    }
+}
+
+async function httpGetFlightById(req, res, next) {
+    try {
+        const flight = await Flight.findByPk();
+        res.status(200).json({success: true, flight})
     } catch(err) {
         console.log(err);
         next(err);
@@ -13,7 +23,7 @@ async function httpGetAllFlights(req, res, next) {
 async function httpCreateNewFlight(req, res, next) {
     try {
         const flight = await Flight.create(req.body);
-        res.json({success: true, message: 'flight successfully scheduled'})
+        res.status(201).json({success: true, message: 'flight successfully scheduled'})
     } catch(err) {
         console.log(err);
         next(err);
