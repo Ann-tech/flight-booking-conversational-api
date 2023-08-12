d28301b42bb8a0ea3d2df4bbfb47890bf7ab0201
Ann-tech <Onyekaann17@gmail.com>
2023-08-12 14:10:13 +0100

    bug fix

--- src/controllers/processRequest.controller.js
+++ src/controllers/processRequest.controller.js
@@ -1,11 +1,5 @@
-const { httpGetAllFlights } = require("./flights.controller")
-
-async function processRequest(req, res) {
-    if (req.queryResult.action == "getAvailableFlights") {
+async function processRequest(req, res, next) {
+    if (req.body.queryResult?.action == "getAvailableFlights") {
         return httpGetAllFlights(req, res, next)
     }
-}
-
-module.exports = {
-    processRequest
 }
