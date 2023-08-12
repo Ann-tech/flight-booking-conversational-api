a3eb3014f1ccc35dad9d70e8c0d28cbc3f72d1bc
Ann-tech <Onyekaann17@gmail.com>
2023-08-12 05:31:47 +0100

    add dialogflow configuration

--- src/index.js
+++ src/index.js
@@ -30,6 +30,25 @@ app.use( helmet() );
 //connect to db
 const { db } = require('./models/db.connect');
 
+/**
+* on this route dialogflow send the webhook request
+* For the dialogflow we need POST Route.
+* */
+app.post('/webhook', (req, res) => {
+    // get agent from request
+    let agent = new WebhookClient({request: req, response: res})
+    // create intentMap for handle intent
+    let intentMap = new Map();
+    // add intent map 2nd parameter pass function
+    intentMap.set('webhook-demo',handleWebHookIntent)
+    // now agent is handle request and pass intent map
+    agent.handleRequest(intentMap)
+})
+function handleWebHookIntent(agent){
+    agent.add("Hello I am Webhook demo How are you...")
+}
+
+
 // Signup and login authentication middleware
 require("./authentication/auth");
 
