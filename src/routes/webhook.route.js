0287547e8789e3ba1843d79247143bef1202c538
Ann-tech <Onyekaann17@gmail.com>
2023-08-12 13:15:14 +0100

    testing connection with diagramflow

--- src/routes/webhook.route.js
+++ src/routes/webhook.route.js
@@ -2,7 +2,10 @@ const express = require('express');
 const webhookRoute = express.Router();
 
 webhookRoute.post('/', (req, res) => {
+    console.log(req.body.result);
     console.log(req);
+    console.log(res);
+    return res.json(result.body);
 })
 
 module.exports = webhookRoute;
