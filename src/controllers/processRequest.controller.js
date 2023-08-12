6dce82b8b92c29ca48454e785b70b6c8cb882093
Ann-tech <Onyekaann17@gmail.com>
2023-08-12 16:15:52 +0100

    bug fix

--- src/controllers/processRequest.controller.js
+++ src/controllers/processRequest.controller.js
@@ -3,8 +3,8 @@ const { Flight } = require('../models/db.connect');
 async function processRequest(req, res, next) {
     if (req.body.queryResult?.action == "getAvailableFlights") {
         try {
-            const flights = await Flight.findAll();
-            console.log("hey, I got here")
+            // const flights = await Flight.findAll();
+            console.log("yello")
             return res.status(200).json({success: true, message: "Here is a list of all flights", flights });
         } catch(err) {
             console.log(err);
